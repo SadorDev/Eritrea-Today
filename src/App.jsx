98f3bc94942b48f8a9b3a7674d69a5ab35d80793
Sador <Zerezghi@live.co.uk>
2024-06-23 13:55:45 +0100

    JSX

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>
+      <h1> The Future lies in the youth </h1>
+      <h2> So we're always concerned about how much they know about Eritrea in the struggle</h2>
+      <h3> The more they know </h3>
+      <h4> The better is the result for the future </h4>
+    </>
+  );
+};
+
+export default App;
