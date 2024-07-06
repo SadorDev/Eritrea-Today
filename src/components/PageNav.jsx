f49e2e6f56953ebc2167ba177346116795abc917
Sador <Zerezghi@live.co.uk>
2024-07-06 01:57:50 +0100

    Added dropdown effect to pagenav with css

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -4,31 +4,48 @@ import styles from "./PageNav.module.css";
 const PageNav = () => {
   return (
     <nav className={styles.nav}>
-     
       <ul className={styles.navLinks}>
-        <li>
+        <li className={styles.navItem}>
           <NavLink to="/" className={styles.navLink}>Home</NavLink>
         </li>
-        <li>
-          <NavLink to="/Articles" className={styles.navLink}>Articles</NavLink>
+
+        <li className={styles.navItem}>
+          <NavLink to="/articles" className={styles.navLink}>Articles</NavLink>
+          <ul className={styles.dropdown}>
+            <li> <NavLink to="/articles/artsandsports" className={styles.navLink}> Arts and Sports</NavLink> </li>
+            <li> <NavLink to="/articles/general" className={styles.navLink}> General </NavLink> </li>
+            <li> <NavLink to="/articles/nationbuilding" className={styles.navLink}> Nation Building</NavLink> </li>
+          </ul>
         </li>
-        <li>
-          <NavLink to="/AboutEritrea" className={styles.navLink}>About Eritrea</NavLink>
+
+        <li className={styles.navItem}>
+          <NavLink to="/abouteritrea" className={styles.navLink}>About Eritrea</NavLink>
+          <ul className={styles.dropdown}>
+            <li><NavLink to="/abouteritrea/erina" className={styles.navLink}>Erina</NavLink></li>
+            <li><NavLink to="/abouteritrea/historyandculture" className={styles.navLink}>History and Culture</NavLink></li>
+            <li><NavLink to="/abouteritrea/proverbs" className={styles.navLink}>Proverbs</NavLink></li>
+            <li><NavLink to="/abouteritrea/todayinhistory" className={styles.navLink}>Today in History</NavLink></li>
+          </ul>
         </li>
-        <li>
-          <NavLink to="/Editorial" className={styles.navLink}>Editorial</NavLink>
+
+        <li className={styles.navItem}>
+          <NavLink to="/editorial" className={styles.navLink}>Editorial</NavLink>
         </li>
-        <li>
-          <NavLink to="/PressRelease" className={styles.navLink}>Press Release</NavLink>
+        <li className={styles.navItem}>
+          <NavLink to="/pressrelease" className={styles.navLink}>Press Release</NavLink>
         </li>
-        <li>
-          <NavLink to="/Newspapers" className={styles.navLink}>Newspapers</NavLink>
+        <li className={styles.navItem}>
+          <NavLink to="/newspapers" className={styles.navLink}>Newspapers</NavLink>
         </li>
-        <li>
-          <NavLink to="/Magazines" className={styles.navLink}>Magazines</NavLink>
+        <li className={styles.navItem}>
+          <NavLink to="/magazines" className={styles.navLink}>Magazines</NavLink>
         </li>
-        <li>
-          <NavLink to="/More" className={styles.navLink}>More</NavLink>
+
+        <li className={styles.navItem}>
+          <NavLink to="/more" className={styles.navLink}>More</NavLink>
+          <ul className={styles.dropdown}>
+            <li> <NavLink to="/more/contactus" className={styles.navLink}>Contact Us </NavLink> </li>
+          </ul>
         </li>
       </ul>
     </nav>
