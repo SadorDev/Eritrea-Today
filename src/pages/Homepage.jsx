a1d8269d0af1c7120fa478b76d4c87cc8909381f
Sador <Zerezghi@live.co.uk>
2024-07-14 07:55:29 +0100

    Fetched homepage data from API, styling to be done*

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,40 @@
-import AppLayout from './AppLayout.jsx';
-import styles from './Homepage.module.css'; // Import specific page CSS module
+import { useEffect, useState } from "react";
+
+import styles from "./Homepage.module.css";
+import AppLayout from "./AppLayout.jsx";
 
 const Homepage = () => {
+  const BASE_URL = "http://localhost:8000";
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    async function fetchNews() {
+      try {
+        const res = await fetch(`${BASE_URL}/topStories`);
+        const data = await res.json();
+        setNews(data);
+      } catch {
+        alert("There was an error loading the data");
+      }
+    }
+    fetchNews();
+  }, []);
+
   return (
-    <AppLayout>
-      <main className={styles.headerContainer}>
-        <h3>Welcome to the Homepage</h3>
-        <p>This is where your homepage content goes.</p>
-      </main>
-    </AppLayout>
+    <main className={styles.headerContainer}>
+      <AppLayout />
+      <div>
+        <ul>
+          {news.map((story) => (
+            <li key={story.id}>
+              <h2>{story.title}</h2>
+              <p>{story.summary}</p>
+              <img src={story.image} alt={story.title} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
   );
 };
 
