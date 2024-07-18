1363ba37d0b64340c9f2c38c1e865072a59c0b8d
Sador <Zerezghi@live.co.uk>
2024-07-18 07:48:49 +0100

    Proverbs added & npm run all

--- src/pages/AboutEritrea/SubPages/Proverbs.jsx
+++ src/pages/AboutEritrea/SubPages/Proverbs.jsx
@@ -1,10 +1,39 @@
-import AppLayout from "../../AppLayout"
+import { useEffect, useState } from "react";
+import AppLayout from "../../AppLayout";
+import styles from "./Proverbs.module.css";
 
 const Proverbs = () => {
+  const BASE_URL = "http://localhost:8001";
+
+  const [proverb, setProverb] = useState([]);
+
+  useEffect(() => {
+    async function fetchProverb() {
+      try {
+        const res = await fetch(`${BASE_URL}/proverbs`);
+        const data = await res.json();
+        setProverb(data);
+        console.log(data);
+      } catch {
+        alert("An error was made fetching the proverbs api");
+      }
+    }
+    fetchProverb();
+  }, []);
+
   return (
+    <>
+      <AppLayout />
+      <div className={styles.container}>
+        <div className={styles.test}>
+          <div key={proverb.id}>
+            
 
-    <AppLayout />
-  )
-}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Proverbs
+export default Proverbs;
